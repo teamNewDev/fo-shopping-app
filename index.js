5fced0cd1a565f47d954b739cc2fefe1a1310f4b
Franklyn <franklynwisdom@gmail.com>
2021-11-29 22:22:47 +0100

    implemented the clear all feature, the edit and update feature in the shopping app

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const db = new Dexie('ShoppingApp')
-db.version(1).stores( {items: '++id,name,price,isPurchased'})
+db.version(2).stores( {items: '++id,name,price,quantity,isPurchased'})
 
 const itemForm = document.getElementById('itemForm')
 const itemsDiv = document.getElementById('itemsDiv')
 const totalPriceDiv = document.getElementById('totalPriceDiv')
+const clearAllItemButton = document.getElementById('clearAllItemButton')
+const nameInput = document.getElementById('nameInput')
+const quantityInput = document.getElementById('quantityInput')
+const priceInput = document.getElementById('priceInput')
+
 
 const populateItemsDiv = async () => {
     const allItems = await db.items.reverse().toArray()
@@ -26,6 +31,14 @@ const populateItemsDiv = async () => {
 
         <button class="deleteButton" onclick="removeItem(${item.id})">
             X
+        </button>
+
+        <button class="editItemButton" onclick="editItem(event, ${item.id})">
+            Edit item
+        </button>
+
+        <button class="updateItemButton" onclick="updateItem(event, ${item.id})">
+            Update item
         </button>
 
     </div>
@@ -62,6 +75,33 @@ const removeItem = async (id) => {
     await db.items.delete(id)
     await populateItemsDiv()
 }
+
+const clearAllItem = async () => {
+    await db.items.clear()
+    await populateItemsDiv()
+    console.log('deleted all')
+}
+
+const editItem = (event, id) => {
+    nameInput.focus()
+    console.log('new focus');
+    console.log(id);
+    console.log(event.target.value);
+}
+
+const updateItem = async (event, id) => {
+    await db.items.update(id, {name: nameInput.value, price: priceInput.value, quantity: quantityInput.value})
+    console.log('item updated');
+    console.log(event.target.value);
+    console.log(id);
+
+    await populateItemsDiv()
+    itemForm.reset()
+
+
+}
+
+clearAllItemButton.addEventListener('click',clearAllItem)
 
 
 // const g = async (event) => {
